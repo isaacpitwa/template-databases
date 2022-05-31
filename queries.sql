fac3dedde5ed64c7e7b7529179d2c112ea15d392
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-01 02:07:27 +0300

    [ADD] add more data to database

--- queries.sql
+++ queries.sql
@@ -9,3 +9,4 @@ SELECT * FROM animals WHERE neutered ='1'; /* Find all animals that are neutered
 SELECT * FROM animals WHERE  NOT name='Gabumon'; /* Find all animals not named Gabumon.*/
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <=17.3; /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)*/
 
+
