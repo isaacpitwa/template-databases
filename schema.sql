1c440a424b91dc555b5be51eb2c2b51add68891f
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-01 01:40:38 +0300

    [Fix] make id auto increment and enforce NOT NUll

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id int,
+    id int NOT NULL IDENTITY(1,1),
     name  varchar(1000),
     date_of_birth  date,
     escape_attempts int, neutered bit, weight_kg decimal,
