8dc34379247c5c84f107ded09948e4be8fe52ec6
fabianofrank <fabianofrank@protonmail.com>
2022-06-07 17:02:07 -0300

    Diagram based tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,41 @@
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  name varchar(25) NOT NULL,
+  date_of_birth DATE
+)
+
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP DEFAULT NOW(),
+  patient_id int REFERENCES patients(id),
+  status varchar(25) NOT NULL
+)
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type varchar(25) NOT NULL,
+  name varchar(25) NOT NULL  
+)
+
+CREATE TABLE medical_treatments (
+  id SERIAL PRIMARY KEY,
+  medical_histories_id int REFERENCES medical_histories  (id),
+  treatments_id int REFERENCES treatments(id)
+)
+
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  total_amout DECIMAL,
+  generated_at TIMESTAMP DECIMAL NOW(),
+  payed_at TIMESTAMP DECIMAL NOW(),
+  medical_history_id int REFERENCES medical_histories(id)
+)
+
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity int NOT NULL,
+  total_price DECIMAL,
+  invoice_id int REFERENCES invoices(id),
+  treatment_id int REFERENCES  treatments(id)
+)
